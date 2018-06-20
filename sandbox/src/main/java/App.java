99d5a2d52efad22cc94fe36991cd238f452b9136
Nikita D <merryhangedman@gmail.com>
2018-06-20 22:11:58 +0300

    Distance between 2 points calculation

--- sandbox/src/main/java/App.java
+++ sandbox/src/main/java/App.java
@@ -8,5 +8,22 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+    Point p1 = new Point();
+    p1.x = 0;
+    p1.y= 0;
+
+    Point p2= new Point();
+    p2.x = 1;
+    p2.y= 1;
+
+    System.out.println(distance(p1,p2));
+
     }
+
+    public static double distance (Point p1, Point p2){
+        return  Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+    }
+
+
 }
